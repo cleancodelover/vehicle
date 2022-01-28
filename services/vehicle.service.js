3e44cb44071ee0b4a1176d61471649ce2f948cdb
cleancodelover <verkyavpeter@gmail.com>
2022-01-28 14:08:34 +0100

    make changes to test cases

--- services/vehicle.service.js
+++ services/vehicle.service.js
@@ -6,74 +6,8 @@ const VehicleTelemetryService = require("./vehicleTelemetry.service");
 class VehicleService extends VehicleTelemetryService {
   constructor() {
     super();
-    this.helper = new Helpers();
   }
-
-  async getVehicleById(vehicleId) {
-    if (!vehicleId) {
-      return {
-        message: "No vehicle found.",
-        success: false,
-        error: false,
-        data: null,
-      };
-    }
-    try {
-      let result = await db.Vehicle.findOne({
-        where: { id: vehicleId },
-      });
-      if (!result) {
-        return {
-          message: "Vehicle not found.",
-          success: false,
-          error: false,
-          data: null,
-        };
-      }
-      return {
-        message: "Success.",
-        success: true,
-        error: false,
-        data: result,
-      };
-    } catch (error) {
-      logger.error(new Error(error));
-      return {
-        message: "Database error occurred.",
-        success: false,
-        error: true,
-        data: null,
-      };
-    }
-  }
-
-  async getVehicles() {
-    try {
-      let result = await db.Vehicle.findAll();
-      if (!result) {
-        return {
-          message: "There are no vehicles.",
-          success: false,
-          error: false,
-          data: null,
-        };
-      }
-      return {
-        message: "Success.",
-        success: true,
-        error: false,
-        data: result,
-      };
-    } catch (error) {
-      logger.log(new Error(error));
-      return {
-        message: "Database connection error.",
-        success: false,
-        error: true,
-        data: null,
-      };
-    }
-  }
+  //functions are extended from vehicleTelemetry.service.js and vehicleBoundaryEvent.service.js classes.
 }
 
 module.exports = VehicleService;
